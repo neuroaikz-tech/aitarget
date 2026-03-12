8f3913bb118862219dbd6831cb47a57164f3412a
EldarGGG <eldar.usenov08@gmail.com>
2026-03-12 15:37:55 +0500

    fix: increase express body parser limit to 50mb to allow large image uploads

--- server/src/index.ts
+++ server/src/index.ts
@@ -44,8 +44,8 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 
 app.use(
